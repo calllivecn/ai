1965996d97a2536f0d065239b94f2bc3c8358e48
calllivecn <c-all@qq.com>
2019-01-21 18:53:08 +0800

    修改：convnet.py

--- convnet.py
+++ convnet.py
@@ -18,7 +18,7 @@ model.add(layers.MaxPooling2D((2, 2)))
 
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
-model.summary()
+#model.summary()
 
 
 model.add(layers.Flatten())
@@ -27,13 +27,13 @@ model.add(layers.Dense(64, activation='relu'))
 
 model.add(layers.Dense(10, activation='softmax'))
 
-model.summary()
+model.summary()#;exit(0)
 
 
 from keras.datasets import mnist
 from keras.utils import to_categorical
 
-(train_iamges, train_labels) , (test_images, test_labels) = mnist.load_data()
+(train_images, train_labels) , (test_images, test_labels) = mnist.load_data()
 
 train_images = train_images.reshape((60000, 28, 28 ,1))
 train_images = train_images.astype('float32') / 255
@@ -49,9 +49,8 @@ model.compile(optimizer='rmsprop',
                 metrics=['accuracy']
                 )
 
-model.fit(train_iamges, train_labels, epochs=5, batch_size=64)
+model.fit(train_images, train_labels, epochs=5, batch_size=64)
 
 # 在测试数据上对模型进行评估
 test_less, test_acc = model.evaluate(test_images, test_labels)
-
-print(test_acc)
+print(test_less, test_acc)
